86f1f54891c99f1f7d85b8672258b0f5eefd265a
szm7 <96779061+szm7@users.noreply.github.com>
2023-12-12 12:06:24 +0530

    interface example

---
+++ intr1.java
@@ -0,0 +1,22 @@
+interface inf1{
+    static void show(){
+        System.out.println("Interface 1");
+    }
+}
+interface inf2{
+    default void show(){
+        System.out.println("Interface 2");
+    }
+}
+ class test implements inf1,inf2 {
+    public void show(){
+        inf1.show();
+        inf2.super.show();
+    }
+    public static void main(String[] args) {
+        test t =new test();
+        t.show();
+    }
+    
+}
+
