b4bc69501ad02661275d497f9fdb6c2586ca4a7a
szm7 <96779061+szm7@users.noreply.github.com>
2024-01-05 20:55:49 +0530

    fix

--- waterConservation.java
+++ waterConservation.java
@@ -10,56 +10,56 @@ class CityBlockConservation extends RainySeasonConservation {
         if (blockHeights == null || blockHeights.length <= 2) {
             return 0;
         } else {
-            int highestValueLeft = blockHeights[0];
-            int leftIndex = 0;
+            int highestValue = 0;
+            int startIndex = 0;
 
-            int highestValueRight = blockHeights[0];
-            int rightIndex = 0;
+            int endIndex = 0;
+            int secondHighestValue = 0;
 
-            for (int i = 0; i < blockHeights.length / 2; i++) {
-                if (blockHeights[i] > highestValueLeft) {
-                    highestValueLeft = blockHeights[i];
-                    leftIndex = i;
+            for (int i = 0; i < blockHeights.length; i++) {
+                if (blockHeights[i] > highestValue) {
+                    secondHighestValue = highestValue;
+                    highestValue = blockHeights[i];
+                    endIndex = startIndex;
+                    startIndex = i;
+                } else if (blockHeights[i] > +secondHighestValue) {
+                    secondHighestValue = blockHeights[i];
+                    endIndex = i;
                 }
             }
+            System.out.println(highestValue + " " + secondHighestValue);
+            System.out.println(endIndex + " " + startIndex);
 
-            for (int i = blockHeights.length - 1; i >= blockHeights.length / 2; i--) {
-                if (blockHeights[i] > highestValueRight) {
-                    highestValueRight = blockHeights[i];
-                    rightIndex = i;
-                }
-            }
+            // System.out.println(blockHeights.length / 2);
+            // for (int i = blockHeights.length / 2; i < blockHeights.length; i++) {
+            // if (blockHeights[i] > highestValueRight) {
+            // highestValueRight = blockHeights[i];
+            // rightIndex = i;
+            // }
+            // }
+
+            int highestVal = highestValue > secondHighestValue ? secondHighestValue : highestValue;
             int tappedWater = 0;
-            for (int i = leftIndex; i < rightIndex; i++) {
-                tappedWater += highestValueLeft - blockHeights[i];
+            if (startIndex < endIndex) {
+                int temp = endIndex;
+                endIndex = startIndex;
+                startIndex = temp;
+            }
+            System.out.println(endIndex + " " + startIndex);
+            for (int i = endIndex + 1; i < startIndex; i++) {
+                tappedWater += secondHighestValue - blockHeights[i];
+                System.out.println(secondHighestValue - blockHeights[i]);
 
             }
             return tappedWater;
         }
-        // int n = blockHeights.length;
-        // int[] l_block_Height = new int[n];
-        // int[] r_block_Height = new int[n];
-        // l_block_Height[0] = blockHeights[0];
-        // for (int i = 1; i < n; i++) {
-        // l_block_Height[i] = Math.max(l_block_Height[i - 1], blockHeights[i]);
-        // }
-        // r_block_Height[n - 1] = blockHeights[n - 1];
-        // for (int i = n - 2; i >= 0; i--) {
-        // r_block_Height[i] = Math.max(r_block_Height[i + 1], blockHeights[i]);
-        // }
-        // int trappedWater = 0;
-        // for (int i = 0; i < n; i++) {
-        // int minHeight = Math.min(l_block_Height[i], r_block_Height[i]);
-        // trappedWater += Math.max(0, minHeight - blockHeights[i]);
-        // }
-        // return trappedWater;
     }
 }
 
 public class waterConservation {
     public static void main(String[] args) {
         CityBlockConservation c = new CityBlockConservation();
-        int[] blockHeights = { 3,  0, 2, 0, 4 };
+        int[] blockHeights = { 0, 0, 0, 2, 0, 3 };
         int trappedWater = c.calculateTrappedWater(blockHeights);
         System.out.println("Trapped Water: " + trappedWater);
     }
